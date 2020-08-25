2f3336fa8e8994f9a8265f9522079c1a0587f2a7
Saichethan M. Reddy <saichethanreddymiriyala@gmail.com>
2020-08-25 13:43:35 +0530

    Update load.py

--- load.py
+++ load.py
@@ -8,9 +8,9 @@ import pickle
 
 
 def load_data():
-	albert_train = pickle.load(open('embeddings/bert_train.np', 'rb'))
-	albert_val = pickle.load(open('embeddings/bert_val.np', 'rb'))
-	albert_test = pickle.load(open('embeddings/bert_test.np', 'rb'))
+	bert_train = pickle.load(open('embeddings/bert_train.np', 'rb'))
+	bert_val = pickle.load(open('embeddings/bert_val.np', 'rb'))
+	bert_test = pickle.load(open('embeddings/bert_test.np', 'rb'))
 	elmo_train = pickle.load(open('embeddings/elmo_train.np', 'rb'))
 	elmo_val = pickle.load(open('embeddings/elmo_val.np', 'rb'))
 	xlnet_train = pickle.load(open('embeddings/xlnet_train.np', 'rb'))
